79cd7485d85e78f013eb29b0a2c1779f8fcd73f0
agus_prayogi <agusyogi600@gmail.com>
2020-08-26 09:09:29 +0700

    (+) tugas 1

---
+++ Arimatika/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        TextBox2.Text = TextBox1.Text * 0.8
+        TextBox3.Text = TextBox1.Text + 273.15
+        TextBox4.Text = TextBox1.Text * 1.8 + 32
+    End Sub
+End Class
